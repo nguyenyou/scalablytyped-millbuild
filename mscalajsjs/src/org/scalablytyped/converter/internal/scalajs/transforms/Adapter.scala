ee7ee192e2e3eb24fd808bf6fb8fceb86740eb96
nguyenyou <tunt081295@gmail.com>
2025-08-25 22:09:23 +0700

    🚢 ship it

---
+++ mscalajsjs/src/org/scalablytyped/converter/internal/scalajs/transforms/Adapter.scala
@@ -0,0 +1,9 @@
+package org.scalablytyped.converter.internal
+package scalajs
+package transforms
+
+/** Some of the transformations were written before we added the `typings` outermost package.
+  *  This maintains that somewhat simpler world view */
+object Adapter {
+
+}
