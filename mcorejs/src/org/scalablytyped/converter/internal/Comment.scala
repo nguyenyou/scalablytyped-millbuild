803524bc0f300d088e333d0ae821801209b077dd
nguyenyou <tunt081295@gmail.com>
2025-08-25 18:57:21 +0700

    🚢 ship it

--- mcorejs/src/org/scalablytyped/converter/internal/Comment.scala
+++ mcorejs/src/org/scalablytyped/converter/internal/Comment.scala
@@ -1,7 +1,8 @@
 package org.scalablytyped.converter.internal
 
 import io.circe.{Decoder, Encoder}
-import org.scalablytyped.converter.internal.scalajs.QualifiedName
+import org.scalablytyped.converter.internal.scalajs.{ExprTree, QualifiedName, TypeRef}
+import org.scalablytyped.converter.internal.ts.TsIdentModule
 
 sealed trait Comment
 
@@ -19,7 +20,17 @@ object Marker {
   case object EnumObject extends Marker
   case object HasClassParent extends Marker
 
-  case class NameHint(value: String) extends Marker
+  case class NameHint(value:        String) extends Marker
+  case class ModuleAliases(aliases: IArray[TsIdentModule]) extends Marker
+  case class WasLiteral(lit:        ExprTree.Lit) extends Marker
+  case class WasUnion(related:      IArray[TypeRef]) extends Marker
+
+  /* Disable the minimizer for object with this marker */
+  final case class MinimizationKeep(related: IArray[TypeRef]) extends Marker
+
+  /* Similar to above, but it's conditional. If the object with this marker is included, only then include the related objects as well */
+  final case class MinimizationRelated(related: IArray[TypeRef]) extends Marker
+
   case class WasDefaulted(among: Set[QualifiedName]) extends Marker
 
   case object ManglerLeaveAlone extends Marker
@@ -32,12 +43,8 @@ object Comment {
   def apply(raw: String): Comment = Comment.Raw(raw)
 
   def warning(s: String)(implicit e: sourcecode.Enclosing): Comment =
-    Comment(
-      s"/* import warning: ${e.value.split("\\.").takeRight(2).mkString(".")} $s */"
-    )
+    Comment(s"/* import warning: ${e.value.split("\\.").takeRight(2).mkString(".")} $s */")
 
-  implicit lazy val encodes: Encoder[Comment] =
-    io.circe.generic.semiauto.deriveEncoder
-  implicit lazy val decodes: Decoder[Comment] =
-    io.circe.generic.semiauto.deriveDecoder
+  implicit lazy val encodes: Encoder[Comment] = io.circe.generic.semiauto.deriveEncoder
+  implicit lazy val decodes: Decoder[Comment] = io.circe.generic.semiauto.deriveDecoder
 }
